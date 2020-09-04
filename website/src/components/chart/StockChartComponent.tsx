64e769c9058d4e50f7faf08e75329249960fe6bb
Choozy96 <47892948+Choozy96@users.noreply.github.com>
2020-09-05 00:31:08 +0800

    added sample time series chart

---
+++ website/src/components/chart/StockChartComponent.tsx
@@ -0,0 +1,25 @@
+
+import Plot from 'react-plotly.js';
+import React from 'react';
+
+interface IProps {
+    data?: any
+}
+
+const StockChartComponent = (props: IProps) => {
+    
+    return (
+        <Plot
+          data={[
+            {
+              x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+              y: [1, 3, 6],
+              type: 'scatter'
+            }
+          ]}
+          layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+        />
+      );
+}
+
+export {StockChartComponent};
---
+++ website/src/pages/chart.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NextComponentType, NextPageContext, GetServerSideProps } from 'next'
+import styles from './login.module.scss';
+import dynamic from 'next/dynamic'
+
+const StockChartComponent = dynamic(() =>
+  import('../components/chart/StockChartComponent').then((mod) => mod.StockChartComponent)
+)
+
+
+const login: NextComponentType<NextPageContext, any> = () => {
+
+    return (
+        <div className={styles.container}>
+            <StockChartComponent />
+        </div>
+    );
+}
+
+export default login;
