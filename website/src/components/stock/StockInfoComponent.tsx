1931a4b8ced5ae0084913c2c93f392363c49c520
Cedric Lee Shao Ming <cedric.lee.2018@sis.smu.edu.sg>
2020-09-06 12:57:49 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/CedricLSM/Team3Pump""
    This reverts commit 0abfc2bd441b35189d95005117857a5b52b57725.

--- website/src/components/stock/StockInfoComponent.tsx
+++ website/src/components/stock/StockInfoComponent.tsx
@@ -1,11 +1,11 @@
 
-import React, { useState, MouseEvent } from 'react';
-import { Card, Table, Button } from 'react-bootstrap';
+import React, { useState, MouseEvent, FormEvent } from 'react';
+import { Card, Table, Button, Form, Row, Col, Alert } from 'react-bootstrap';
 import { AiOutlineEdit } from "react-icons/ai";
 
 interface IProps {
     ticker: string,
-    changeTicker: (ticker: string) => void;
+    setTicker: any;
     info?: any,
     news?: any
 }
@@ -13,17 +13,51 @@ interface IProps {
 const StockInfoComponent = (props: IProps) => {
 
     const [edit, setEdit] = useState<boolean>(false);
+    const [newTicker, setNewTicker] = useState<string>("");
     
-    const onClick = (e: MouseEvent<HtmlElement, MouseEvent>) => {
+    const onClick = () => {
         setEdit(true);
     }
+
+    const cancelSearch = () => {
+        setEdit(false);
+    }
+
+    const handleSubmit = ((e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setEdit(false);
+        props.setTicker(newTicker);
+    })
+
+    console.log(props.news);
 
     return (
         <Card>
             <Card.Body>
-                <Card.Title className="text-uppercase font-weight-bold">{props.ticker}<Button onClick={onClick()}><AiOutlineEdit /></Button></Card.Title>
+                <Card.Title className="text-uppercase font-weight-bold">
+                    {edit ?
+                    <>
+                        <Form onSubmit={handleSubmit}>
+                            <Row>
+                                <Form.Group as={Col} sm="4">
+                                        <Form.Control type="text" onChange={e => setNewTicker(e.target.value)}/>
+                                </Form.Group>
+                                <Col sm="2">
+                                        <Button type="submit" variant="light">Search</Button>
+                                    </Col>
+                                    <Col sm="2">
+                                        <Button variant="danger" onClick={cancelSearch}>Cancel</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </>
+                    :
+                    <>
+                        {props.ticker}<Button variant="link" onClick={onClick}><AiOutlineEdit /></Button>
+                    </>}
+                </Card.Title>
                 {
-                    props.info ?
+                    (props.info.bid && props.info.ask && props.news != "No data found") ?
                     <Card.Text>
                         <Table>
                             <tr>
@@ -51,7 +85,7 @@ const StockInfoComponent = (props: IProps) => {
                         </Table>
                     </Card.Text>
                     :
-                    null
+                    <Alert variant="warning">No Stock Information Found!</Alert>
                 }
                 
             </Card.Body>
